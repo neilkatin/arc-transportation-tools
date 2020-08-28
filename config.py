ea14802aa2fbabcfb117b98b40353ef7c4927d69
Neil Katin <github2@askneil.com>
2020-08-27 18:39:48 -0700

    added reconciling with AVIS report

--- config.py
+++ config.py
@@ -2,13 +2,21 @@
 
 SRC_DIR="/cygdrive/c/users/neil/Downloads"
 
-STAFF_ROSTER = f"{ SRC_DIR }/Staff Roster 8.26 630AM.xlsx"
+STAFF_ROSTER = f"{ SRC_DIR }/Staff Roster 8-27 900 AM.xlsx"
 STAFF_ROSTER_SHEET_NAME = "Staff Roster"
 
 VEHICLES = f"{ SRC_DIR }/Vehicles.xlsx"
 VEHICLES_SHEET_NAME = "Sheet1"
 
+OPEN_RENTALS = f"{ SRC_DIR }/ARC Open Rentals - 08-26-2020.xlsx"
+OPEN_RENTALS_SHEET_NAME = "Open RA"
+OPEN_RENTALS_DRS = [ "534", "534-2021", 'DR534', 'DR534-21' ]
+OPEN_RENTALS_TITLE_ROW = 3
+
 #DST_DIR = SRC_DIR
 DST_DIR = "."
-MERGED = f"{ DST_DIR }/merged.xlsx"
+OUTPUT_WB = f"{ DST_DIR }/merged.xlsx"
 MERGED_SHEET_NAME = "Merged"
+RECONCILED_SHEET_NAME = "Reconciled"
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
 import logging
 import argparse
@@ -10,6 +11,8 @@ import init_logging
 
 import openpyxl
 import openpyxl.utils
+import openpyxl.styles
+import openpyxl.styles.colors
 import dotenv
 
 import config as config_static
@@ -68,20 +71,37 @@ def main():
     staff_ws = staff_wb[config.STAFF_ROSTER_SHEET_NAME]
     staff_map = build_map(staff_ws, 4, "Name", empty_filter)
 
-    merged_file = config.MERGED
-    if os.path.exists(merged_file):
+    # delete workbook if it exists
+    output_file = config.OUTPUT_WB
+    if os.path.exists(output_file):
         # eventually copy fields out...
-        os.remove(merged_file)
+        os.remove(output_file)
 
-    merged_wb = openpyxl.Workbook()
-    merged_ws = merged_wb.active
+    # create a new one
+    output_wb = openpyxl.Workbook()
+
+    # handle the merged sheet
+    merged_ws = output_wb.active
     merged_ws.title = config.MERGED_SHEET_NAME
 
-    combine_data(merged_ws, vehicles_map, staff_map)
+    make_merged(merged_ws, vehicles_map, staff_map)
 
-    merged_wb.save(merged_file)
+    # handle reconciliation; ok if open_rentals_files isn't there; just don't make the sheet if it isn't
+    rentals_file = config.OPEN_RENTALS
+    if os.path.exists(rentals_file):
+        rentals_wb = openpyxl.load_workbook(rentals_file)
+        rentals_ws = rentals_wb[config.OPEN_RENTALS_SHEET_NAME]
 
-def combine_data(out_ws, vehicles_map, staff_map):
+        reconciled_ws = output_wb.create_sheet(title=config.RECONCILED_SHEET_NAME)
+
+        make_reconciled(reconciled_ws, rentals_ws, config.OPEN_RENTALS_TITLE_ROW, vehicles_ws, 1, config.OPEN_RENTALS_DRS)
+
+    output_wb.save(output_file)
+
+
+
+def make_merged(out_ws, vehicles_map, staff_map):
+    """ generate the merged sheet from vehicles and staff """
 
     # first make the title row
     vehicles_columns = [ 'Name', 'Reservation No', 'GAP', 'Date Received', ]
@@ -133,18 +153,181 @@ def combine_data(out_ws, vehicles_map, staff_map):
                 out_ws.cell(row=rownum, column=out_index, value=value)
 
 
+def make_reconciled(reconciled_ws, rentals_ws, rentals_starting_row, vehicles_ws, vehicles_starting_row, dr_list):
+    """ generate reconciled ws from rentals_ws, marking which key numbers and reservation numbers are in vehicles_ws """
+    log.debug("make_reconciled: called")
+    
+    rentals_name_map, rentals_cols = process_title_row(rentals_ws, rentals_starting_row)
+    vehicles_name_map, vehicles_cols = process_title_row(vehicles_ws, vehicles_starting_row)
 
-def build_map(sheet, starting_row, key_name, row_filter):
-    # grab the title row
-    for row in sheet.iter_rows(min_row=starting_row, max_row=starting_row, values_only=False):
-        title_row = row
-        title_name_map = {}
-        title_cols = {}
-        for cell in title_row:
+    #log.debug(f"rentals_name_map: { rentals_name_map }")
+
+    # go through the vehicles sheet and gather up all the reservation and key numbers.
+    key_map = gather_column(vehicles_ws, vehicles_name_map['Key'], vehicles_starting_row, "Vehicles", 'Key')
+    reservation_map = gather_column(vehicles_ws, vehicles_name_map['Reservation No'], vehicles_starting_row, "Vehicles", 'Reservation No')
+    plate_map = gather_column(vehicles_ws, vehicles_name_map['Plate'], vehicles_starting_row, "Vehicles", 'Plate')
+
+    # cell colors we'll be using
+    fill_red = openpyxl.styles.PatternFill(fgColor="FFC0C0", fill_type = "solid")
+    fill_green = openpyxl.styles.PatternFill(fgColor="C0FFC0", fill_type = "solid")
+    fill_yellow = openpyxl.styles.PatternFill(fgColor="FFFFC0", fill_type = "solid")
+
+    plate_column = rentals_name_map['License Plate Number']
+    key_column = rentals_name_map['MVA No']
+    res_column = rentals_name_map['Reservation No']
+    drs_column = rentals_name_map['Cost Control No']
+
+    log.debug("before output generation")
+
+    # turn the list of drs into a dict for easy matching
+    dr_map = {}
+    for item in dr_list:
+        dr_map[item] = 1
+
+    column_dims = reconciled_ws.column_dimensions
+
+    column_widths = {
+            'Rental Region Desc': 15,
+            'Rental Zone Desc': 15,
+            'Rental Distict Desc': 10,
+            'MVA No': 10,
+            'License Plate State Code': 5,
+            'License Plate Number': 10,
+            'Make': 6,
+            'Model': 6,
+            'Ext Color Code': 6,
+            'Reservation No': 15,
+            'Rental Agreement No': 15,
+            'CO Date': 12,
+            'CO Time': 10,
+            'Rental Loc Mnemonic': 15,
+            'Address Line 1': 10,
+            'Address Line 3': 10,
+            'Full Name': 20,
+            'Return Loc Mnomonic': 5,
+            'Exp CI Loc Id': 5,
+            'Exp CI Date': 12,
+            'Exp CI Time': 10,
+            'AWD Orgn Buildup Desc': 5,
+            'Cost Control No': 10,
+            'Booking Source Emp no': 10
+            }
+
+    for column_name in column_widths:
+        if column_name in rentals_name_map:
+            column_width = column_widths[column_name]
+            column_index = rentals_name_map[column_name]
+            # -1 in column index is because we're shifting all columns to the left since input has no column A
+            column_dims[openpyxl.utils.get_column_letter(column_index-1)].width = column_width
+
+    # specify date columns
+    date_columns = [ 'CO Date', 'Exp CI Date' ]
+    date_column_map = {}
+    for name in date_columns:
+        date_column_map[name] = 1
+
+
+    # now mark the reconciled_ws with the right colors for items found/not found in vehicles
+    output_row = 0
+    for row in rentals_ws.iter_rows(min_row=rentals_starting_row, values_only=False):
+
+        dr = row[drs_column -1].value
+
+        # filter out unmatched DRs from list
+        if output_row != 0 and dr not in dr_map:
+            continue
+
+        #log.debug(f"output generation row { output_row } dr { dr }")
+
+        output_row += 1
+        for cell in row:
+            # skip blank first column
+            if cell.column == 1:
+                continue
+
+            cell_title = rentals_cols[cell.column]
+
+            #log.debug(f"setting row { output_row } column { cell.column } to '{ cell.value }'")
+
+            # adjust output column by 1 because input sheet has no column 'A'
+            input_column = cell.column
+            output_column = input_column -1
+            value = cell.value
+            out_cell = reconciled_ws.cell(row=output_row, column=output_column, value=value)
+
+            if cell_title in date_column_map:
+                out_cell.number_format = 'yyyy-mm-dd'
+
+            # now fix colors, but not for title row
+            if output_row != 1:
+                if input_column == res_column:
+                    match_map = reservation_map
+                elif input_column == key_column:
+                    match_map = key_map
+                    # trim leading zero from key when matching
+                    value = re.sub('^0', '', value)
+                elif input_column == plate_column:
+                    match_map = plate_map
+                else:
+                    match_map = None
+
+                if match_map != None:
+                    if value in match_map:
+                        out_cell.fill = fill_green
+                    else:
+                        out_cell.fill = fill_red
+
+        # DEBUG ONLY
+        #if output_row > 10:
+        #    break
+
+
+
+
+def gather_column(ws, column_num, title_row_num, table_name, column_name):
+    """ gather all the values in a column into dict for easy matching """
+
+    results = {}
+    row_num = title_row_num
+    for row in ws.iter_rows(min_row=title_row_num+1, min_col=column_num, max_col=column_num, values_only=True):
+        row_num += 1
+        cell = row[0]
+        if cell == '':
+            continue
+
+        # delete formatting characters
+        if isinstance(cell, str):
+            cell = re.sub('[ \t-]', '', cell)
+
+        value = str(cell)
+        if value in results:
+
+            # horrible hack to deal with n/a column in reservation no: just ignore values of n/a
+            if value != 'N/A':
+                # this is a duplicate entry
+                log.error(f"Found duplicate for table { table_name } column { column_name }: old row { results[value] }, new row { row_num }")
+
+        results[value] = row_num
+
+    return results
+
+def process_title_row(sheet, title_row_num):
+    """ build two maps: one mapping column name to column number, and one from column number to name """
+    title_name_map = {}
+    title_cols = {}
+
+    for row in sheet.iter_rows(min_row=title_row_num, max_row=title_row_num, values_only=False):
+        for cell in row:
             #log.debug(f"title { cell.column }, { cell.row } = '{ cell.value }'")
             title_name_map[cell.value] = cell.column
             title_cols[cell.column] = cell.value
         break
+
+    return title_name_map, title_cols
+
+def build_map(sheet, starting_row, key_name, row_filter):
+    # grab the title row
+    title_name_map, title_cols = process_title_row(sheet, starting_row)
 
     #log.debug(f"title_name_map: { title_name_map }")
 
