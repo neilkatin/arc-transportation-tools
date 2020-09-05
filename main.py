a2c2c4c4c4716e6b3d0e75269b570435cfca9e4a
Neil Katin <github2@askneil.com>
2020-09-04 18:44:13 -0700

    fixed bug where cars missing from DTT showed as green; they now show as blue

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ def rentals_filter(row):
 def empty_filter(row):
     return True
 
+def current_filter(row):
+    return row['Ctg'] == 'R' and row['Status'] == 'Active'
+
 
 def main():
     args = parse_args()
@@ -60,7 +63,7 @@ def main():
 
     vehicles_wb = openpyxl.load_workbook(vehicles_file)
     vehicles_ws = vehicles_wb[config.VEHICLES_SHEET_NAME]
-    vehicles_map = build_map(vehicles_ws, 1, "Rcvd From", rentals_filter)
+    vehicles_map = build_map(vehicles_ws, "Vehicles", 1, ["Rcvd From"], rentals_filter)
 
     # delete workbook if it exists
     output_file = config.OUTPUT_WB
@@ -72,6 +75,10 @@ def main():
     output_wb = openpyxl.Workbook()
 
     staff_file = config.STAFF_ROSTER
+    staff_map = None
+    current_ws = None
+    vehicles_spec = [ ['Name', 20 ], ['Reservation No', 15 ], [ 'GAP', 15 ], ['Date Received', 12 ] ]
+    vehicles_spec_len = len(vehicles_spec)
     if not os.path.exists(staff_file):
         log.info(f"skipping { config.MERGED_SHEET_NAME } sheet: could not find staff roster { staff_file }")
     else:
@@ -79,30 +86,79 @@ def main():
         log.debug(f"generating { config.MERGED_SHEET_NAME } sheet using { staff_file }")
         staff_wb = openpyxl.load_workbook(staff_file)
         staff_ws = staff_wb[config.STAFF_ROSTER_SHEET_NAME]
-        staff_map = build_map(staff_ws, 4, "Name", empty_filter)
+        staff_map = build_map(staff_ws, "Staff Roster", config.STAFF_ROSTER_TITLE_ROW, ["Name"], empty_filter)
 
         # handle the merged sheet
         merged_ws = output_wb.create_sheet(title=config.MERGED_SHEET_NAME)
+        merged_ws.freeze_panes = merged_ws['B2']
 
-        make_merged(merged_ws, vehicles_map, staff_map)
+        staff_spec = [ [ 'Email', 30 ], [ 'Cell phone', 14 ], [ 'Assigned', 20 ], [ 'Checked in', 20 ], [ 'Supervisor(s)', 20 ] ]
+
+        vehicles_spec.append([ 'Make', 10 ])
+        vehicles_spec.append([ 'Model', 10 ])
+        vehicles_spec.append([ 'Color', 10 ])
+        vehicles_spec.append([ 'Key', 10 ])
+        vehicles_spec.append([ 'Plate', 10 ])
+        vehicles_spec.append([ 'Tag', 4 ])
+        vehicles_spec.append([ 'Driver', 20 ])
+
+        make_merged(merged_ws, vehicles_map, vehicles_spec, staff_map, staff_spec)
+
+        # handle the active sheet
+        current_ws = output_wb.create_sheet(title=config.CURRENT_SHEET_NAME)
+        current_ws.freeze_panes = current_ws['B2']
+        vehicles_map = build_map(vehicles_ws, "Vehicles", 1, ["Driver","Rcvd From"], current_filter)
+        make_merged(current_ws, vehicles_map, vehicles_spec, staff_map, staff_spec)
+
+
+    outprocessed_file = config.OUTPROCESSED_ROSTER
+    if not os.path.exists(outprocessed_file):
+        log.info(f"skipping { config.OUTPROCESSED_SHEET_NAME } sheet: could not find outprocessed roster file { outprocessed_file }")
+    else:
+        log.debug(f"generating { config.OUTPROCESSED_SHEET_NAME } sheet using { outprocessed_file }")
+
+        outroster_wb = openpyxl.load_workbook(outprocessed_file)
+        outroster_ws = outroster_wb[config.OUTPROCESSED_ROSTER_SHEET_NAME]
+        outroster_map = build_map(outroster_ws, "Outprocessed Roster", config.OUTPROCESSED_ROSTER_TITLE_ROW, ["Name"], empty_filter)
+        outroster_spec = [ [ 'Email', 30 ], [ 'Cell phone', 14 ], [ 'Checked in', 20 ], [ 'Released', 20 ], [ 'Supervisor(s)', 20 ] ]
+
+        out_ws = output_wb.create_sheet(title=config.OUTPROCESSED_SHEET_NAME)
+        out_ws.freeze_panes = out_ws['B2']
+        vehicles_map = build_map(vehicles_ws, "Vehicles", 1, ["Driver","Rcvd From"], current_filter)
+        make_merged(out_ws, vehicles_map, vehicles_spec, outroster_map, outroster_spec, suppress_missing=True)
+
+
+        if staff_map != None:
+            out_ws = output_wb.create_sheet(title=config.MISSING_SHEET_NAME)
+            left_map, right_map = filter_tables(vehicles_map, staff_map, filter_left_only)
+            make_merged(out_ws, left_map, vehicles_spec, outroster_map, outroster_spec)
 
     # handle reconciliation; ok if open_rentals_files isn't there; just don't make the sheet if it isn't
-    rentals_file = config.OPEN_RENTALS
-    if not os.path.exists(rentals_file):
-        log.info(f"skipping { config.RECONCILED_SHEET_NAME } sheet: could not find avis file { rentals_file }")
+    open_file = config.OPEN_RENTALS
+    open_rentals_ws = None
+    closed_rentals_ws = None
+    if not os.path.exists(open_file):
+        log.info(f"skipping { config.RECONCILED_SHEET_NAME } sheet: could not find avis file { open_file }")
     else:
-        log.debug(f"generating { config.RECONCILED_SHEET_NAME } sheet using { rentals_file }")
-        rentals_wb = openpyxl.load_workbook(rentals_file)
-        rentals_ws = rentals_wb[config.OPEN_RENTALS_SHEET_NAME]
+        log.debug(f"generating { config.RECONCILED_SHEET_NAME } sheet using { open_file }")
+        rentals_wb = openpyxl.load_workbook(open_file, read_only=False)
+        open_rentals_ws = rentals_wb[config.OPEN_RENTALS_SHEET_NAME]
+        #open_rentals_ws.reset_dimensions()      # useful for read-only spreadsheets
 
         reconciled_ws = output_wb.create_sheet(title=config.RECONCILED_SHEET_NAME)
+        reconciled_ws.freeze_panes = reconciled_ws['B2']
 
-        make_reconciled(reconciled_ws, rentals_ws, config.OPEN_RENTALS_TITLE_ROW, vehicles_ws, 1, config.OPEN_RENTALS_DRS)
+        make_reconciled(reconciled_ws, open_rentals_ws, config.OPEN_RENTALS_TITLE_ROW, vehicles_ws, 1, config.OPEN_RENTALS_DRS)
 
     # if neither sheet was created: give an error
     if len(output_wb.sheetnames) == 1:
-        log.fatal(f"Neither the AVIS file ({ rentals_file }) nor the staff roster ({ staff_file }) were present.  Aborting...")
+        log.fatal(f"Neither the AVIS file ({ open_file }) nor the staff roster ({ staff_file }) were present.  Aborting...")
         sys.exit(1)
+
+    if current_ws is not None:
+        annotate_vehicles_with_avis(current_ws, vehicles_spec_len, open_rentals_ws, config.OPEN_RENTALS_TITLE_ROW,
+                closed_rentals_ws, config.CLOSED_RENTALS_TITLE_ROW)
+
 
     # delete the initial default sheet
     default_sheet_name = 'Sheet'
@@ -114,15 +170,28 @@ def main():
 
 
 
-def make_merged(out_ws, vehicles_map, staff_map):
-    """ generate the merged sheet from vehicles and staff """
+def make_merged(out_ws, vehicles_map, vehicles_spec, staff_map, staff_spec, suppress_missing=False):
+    """ generate the merged sheet from vehicles and staff
 
-    # first make the title row
-    vehicles_columns = [ 'Name', 'Reservation No', 'GAP', 'Date Received', ]
-    vehicles_col_width = [ 20,    15,              15,     12, ]
-    staff_columns = [ 'Email', 'Cell phone', 'Assigned', 'Checked in', 'Supervisor(s)', ]
-    staff_col_width = [ 30,    14,              20,        20,           20, ]
+        The vehicles_spec and staff_spec control which Columns from the source worksheet are output.
+        Each entry in the array is a 2 element tuple of [ 'Column Name', column_width ].
 
+        suppress_missing means: don't output the line if there is no matching join in the staff_map
+    """
+
+    #log.debug(f"make_merged: vehicles_map size: { len(vehicles_map) }")
+
+    vehicles_columns = []
+    vehicles_col_width = []
+    for e in vehicles_spec:
+        vehicles_columns.append(e[0])
+        vehicles_col_width.append(e[1])
+
+    staff_columns = []
+    staff_col_width = []
+    for e in staff_spec:
+        staff_columns.append(e[0])
+        staff_col_width.append(e[1])
 
     column_map = {}
     column_index = 0
@@ -136,7 +205,7 @@ def make_merged(out_ws, vehicles_map, staff_map):
     for i, name in enumerate(staff_columns):
         column_index += 1
         column_map[name] = column_index
-        column_map[name] = { 'index': column_index, 'map': staff_map, 'col_name': name, 'map_name': 'Staff Roster' }
+        column_map[name] = { 'index': column_index, 'map': staff_map, 'col_name': name, 'map_name': 'Roster' }
         out_ws.cell(row=1, column=column_index, value=name)
         col_dims[openpyxl.utils.get_column_letter(column_index)].width = staff_col_width[i]
 
@@ -147,8 +216,30 @@ def make_merged(out_ws, vehicles_map, staff_map):
     keys = sorted(vehicles_map.keys(), key=str.lower)
 
     rownum = 1
+    in_rownum = 1
     for row_name in keys:
         row = vehicles_map[row_name]
+        failed_row = False
+        in_rownum += 1
+
+        if suppress_missing:
+            # skip the row if not matching columns in roster map
+            found_row = False
+            for name, data in column_map.items():
+                out_index = data['index']
+                in_map = data['map']
+                in_col = data['col_name']
+
+                if in_map != staff_map:
+                    continue
+
+                if row_name in in_map:
+                    found_row = True
+
+            if not found_row:
+                #log.debug(f"Ignoring input row { in_rownum }: no matches on roster")
+                continue
+
         rownum += 1
         for name, data in column_map.items():
             out_index = data['index']
@@ -165,6 +256,47 @@ def make_merged(out_ws, vehicles_map, staff_map):
             else:
                 value = entry[in_col]
                 out_ws.cell(row=rownum, column=out_index, value=value)
+
+
+def filter_tables(left_map, right_map, filter_func):
+    """ filter rows in specified dictionaries with a given filter function
+
+        (part of refactoring: separating row selection from output generation.)
+
+        This function joins the left and right map, then passes the individual entries
+        to the filter function.  If the function returns False then the entry is deleted
+        from the output maps.
+
+        The input maps are not changed; new left and right maps are returned.
+
+        The two maps are assumed to be keyed the same.
+    """
+    left_out = {}
+    right_out = {}
+
+    for key in left_map.keys():
+        left_entry = left_map[key]
+        if key in right_map:
+            right_entry = right_map[key]
+        else:
+            right_entry = None
+
+        if filter_func(left_entry, right_entry):
+            left_out[key] = left_entry
+            if right_entry is not None:
+                right_out[key] = right_entry
+
+    return left_out, right_out
+
+def filter_both(left, right):
+    """ return true if both left and right exist """
+    return left is not None and right is not None
+
+def filter_left_only(left, right):
+    """ return true if both left and right exist """
+    return left is not None and right is None
+
+
 
 
 def make_reconciled(reconciled_ws, rentals_ws, rentals_starting_row, vehicles_ws, vehicles_starting_row, dr_list):
@@ -184,6 +316,7 @@ def make_reconciled(reconciled_ws, rentals_ws, rentals_starting_row, vehicles_ws
     fill_red = openpyxl.styles.PatternFill(fgColor="FFC0C0", fill_type = "solid")
     fill_green = openpyxl.styles.PatternFill(fgColor="C0FFC0", fill_type = "solid")
     fill_yellow = openpyxl.styles.PatternFill(fgColor="FFFFC0", fill_type = "solid")
+    fill_blue = openpyxl.styles.PatternFill(fgColor="C0C0FF", fill_type = "solid")
 
     plate_column = rentals_name_map['License Plate Number']
     key_column = rentals_name_map['MVA No']
@@ -286,6 +419,8 @@ def make_reconciled(reconciled_ws, rentals_ws, rentals_starting_row, vehicles_ws
                 rental_value = row[col -1].value
 
                 # need to clean up data to cannonicalize it
+                if isinstance(rental_value, int):
+                    rental_value = str(rental_value)
                 rental_value = match_fixups[col](rental_value)
                 if rental_value in cmap:
                     match_row = cmap[rental_value]
@@ -295,10 +430,13 @@ def make_reconciled(reconciled_ws, rentals_ws, rentals_starting_row, vehicles_ws
                 #log.debug(f"match: row { output_row } col '{ col }' rental_value '{ rental_value }' match_row '{ match_row }'")
 
             # if all the entries match: mark them green
-            if match_array.count(match_array[0]) == len(match_array):
+            if match_array.count(None) == len(match_array):
+                for col in match_map.keys():
+                    reconciled_ws.cell(row=output_row, column=col-1).fill = fill_blue
+
+            elif match_array.count(match_array[0]) == len(match_array):
                 # all match - mark them all green
                 for col in match_map.keys():
-                    # ZZZ: out_cell[col -1].fill = fill_green
                     reconciled_ws.cell(row=output_row, column=col-1).fill = fill_green
             else:
                 index = 0
@@ -358,15 +496,17 @@ def process_title_row(sheet, title_row_num):
 
     return title_name_map, title_cols
 
-def build_map(sheet, starting_row, key_name, row_filter):
+def build_map(sheet, sheet_name, starting_row, key_name_list, row_filter):
     # grab the title row
     title_name_map, title_cols = process_title_row(sheet, starting_row)
 
     #log.debug(f"title_name_map: { title_name_map }")
 
     results = {}
+    row_num = starting_row
     for row in sheet.iter_rows(min_row=starting_row+1, values_only=False):
-        row_map = {}
+        row_num += 1
+        row_map = { 'row_num': row_num, 'sheet_name': sheet_name }
         for cell in row:
             value = cell.value
             title = title_cols[cell.column]
@@ -376,14 +516,44 @@ def build_map(sheet, starting_row, key_name, row_filter):
             continue
 
         #log.debug(f"row_map: { row_map }")
-        key = row_map[key_name]
+        for name in key_name_list:
+            key = row_map[name]
+            if key != '' and key != None:
+                break
+
+        if key == '' or key == None:
+            log.error(f"build_map: empty key from { key_name_list } for row { row_num }")
 
         if key in results:
-            raise Exception(f"Error: key { key } already in results dictionary")
+            log.error(f"Error: duplicate entry for entry { key } on row { row_num } and { results[key]['row_num'] }")
 
         results[key] = row_map
 
     return results
+
+
+def annotate_vehicles_with_avis(current_ws, insert_col, open_ws, open_starting_row, closed_ws, closed_starting_row):
+
+    current_ws.insert_cols(insert_col + 1)
+    current_ws.cell(row=1, column=insert_col + 1, value="Avis")
+
+    #for i in range(1,  current_ws.max_column +1):
+    #    log.debug(f"after insert column { openpyxl.utils.get_column_letter(i) } to { col_dims[openpyxl.utils.get_column_letter(i)].width }")
+
+    # inserting a column doesn't shift column widths; do so
+    col_dims = current_ws.column_dimensions
+    for i in range(current_ws.max_column + 1, insert_col + 1, -1):
+        col_dims[openpyxl.utils.get_column_letter(i)].width = col_dims[openpyxl.utils.get_column_letter(i -1)].width
+        #log.debug(f"set column { openpyxl.utils.get_column_letter(i) } to { col_dims[openpyxl.utils.get_column_letter(i)].width }")
+
+    current_name_map, current_cols = process_title_row(current_ws, 1)
+
+    if open_ws is not None:
+        open_map, open_cols = process_title_row(open_ws, open_starting_row)
+
+        
+
+
 
 
 
